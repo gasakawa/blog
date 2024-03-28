da5ab1d909f115ea42f7de029682d0da8ff54682
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2024-03-28 19:03:12 -0300

    move post from json file to a array

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,17 +1,17 @@
 import { PostType } from '@/types/post.type';
-import postData from '../posts.json';
 import _ from 'lodash';
+import { posts } from './posts';
 
 export function getPosts(pg = 0): PostType[] {
   const rc = Number(process.env.POST_PER_PAGE);
-  const postList = _.chunk(postData, rc);
+  const postList = _.chunk(posts, rc);
 
   return postList[pg] as PostType[];
 }
 
 export function getPostsByTag(tag: string, pg = 0) {
   const rc = Number(process.env.POST_PER_PAGE);
-  const postsByTag = postData.filter(post => post.tags?.includes(tag));
+  const postsByTag = posts.filter(post => post.tags?.includes(tag));
 
   const postList = _.chunk(postsByTag, rc);
 
@@ -20,7 +20,7 @@ export function getPostsByTag(tag: string, pg = 0) {
 
 export function getPostsByCategory(tag: string, pg = 0) {
   const rc = Number(process.env.POST_PER_PAGE);
-  const postsByCategory = postData.filter(post => post.category?.includes(tag));
+  const postsByCategory = posts.filter(post => post.category?.includes(tag));
 
   const postList = _.chunk(postsByCategory, rc);
 
